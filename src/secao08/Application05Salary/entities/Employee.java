af095261a84284db1df6054a94f964544f11c749
GasparPSousa <gasparufrj@gmail.com>
2021-09-07 20:30:59 -0300

    Criando uma aplicacao para atualizar o salario de um funcionario

---
+++ src/secao08/Application05Salary/entities/Employee.java
@@ -0,0 +1,23 @@
+package secao08.Application05Salary.entities;
+
+public class Employee {
+
+    public String name;
+    public double grossSalary;
+    public double tax;
+
+    public double netSalary() {
+        return grossSalary - tax;
+    }
+
+    public void increaseSalary(double percentage) {
+        this.grossSalary += (percentage/100) * this.grossSalary;
+    }
+
+    @Override
+    public String toString() {
+        return name
+                + ", $ "
+                + String.format("%.2f", netSalary());
+    }
+}
