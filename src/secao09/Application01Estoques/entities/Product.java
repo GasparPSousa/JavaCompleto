b26eca3bd6e21f5061d5fd0727e7cecd7ef513d5
GasparPSousa <gasparufrj@gmail.com>
2021-09-12 10:29:50 -0300

    Gerando o Encapsulamente e usando os metodos getters e setters

--- src/secao09/Application01Estoques/entities/Product.java
+++ src/secao09/Application01Estoques/entities/Product.java
@@ -2,9 +2,9 @@ package secao09.Application01Estoques.entities;
 
 public class Product {
 
-    public String name;
-    public double price;
-    public int quantity;
+    private String name;
+    private double price;
+    private int quantity;
 
     public Product(){
     }
@@ -19,6 +19,33 @@ public class Product {
         this.name = name;
         this.price = price;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+//    public void setQuantity(int quantity) {
+//        this.quantity = quantity;
+//    } // Nesse caso, não vou criar o método setQuantity. Pois dessa forma, vou proteger meu objeto product de alterações INCONSISTENTES na quantidade dele.
+        // A quantidade de produtos SÓ PODE MUDAR por meio das operações entrada e saída do Estoque.
+        // Dessa forma eu consigo proteger que a quantidade do meu produto sempre esteja em um estado consistente.
+        // Ela só vai mudar quando houver uma entrada ou uma saída e não diretamente pelo método setQuantity
 
     public double totalValueInStock() {
         return price * quantity;
